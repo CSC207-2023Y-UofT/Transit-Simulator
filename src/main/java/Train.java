fd75fd504ea53e9f6a5e6a6f2c9c3fd306d581fd
Charles Cheung <charlescheung22@gmail.com>
2023-07-11 17:07:20 -0400

    modifications

--- src/main/java/Train.java
+++ src/main/java/Train.java
@@ -14,51 +14,58 @@ import java.util.ArrayList;
 class Train {
 
     // constructor type params
-    int capacity;  // max capacity
+    private final int capacity;  // max capacity
     /**
-     *  Either "Online", "Maintenance" or "Offline". Precondition: train in "Maintenance" cannot be online
+     *  Either ONLINE, MAINTENANCE or OFFLINE. Precondition: train in MAINTENANCE cannot be ONLINE
      *  Trains that are running can pass Offline trains whether at tracks or at stations. IRL justification: most
      *  tracks have spare tracks that others can pass
      */
-    private final HashSet<String> STATUS_TYPES = new HashSet<> (Arrays.asList("Online", "Maintenance", "Offline"));
-    String status;
+    public enum StatusType {
+        ONLINE,
+        MAINTENANCE,
+        OFFLINE
+    }
+    StatusType status;
     int line;  // line number
     boolean direction;  // true for forwards, false for backwards
     // dict mapping from staff type to an arraylist of staff assigned to that role
-    private HashMap<String, ArrayList<Staff>> staff;
+    private Map<String, List<Staff>> staff;
     private int occupancy;
-
-    // Debatable information can be removed if needed:
-    Node currentLocation;  // assuming that both Station and Track objects can be referenced by this
-    Station nextStation;
+    Node currentLocation;  // assuming that both Station and Track objects can be referenced by this "Node"
 
 
     // constructor
     public Train (int capacity, int line, boolean direction, Node currentLocation, Map env) {
         this.occupancy = 0;
         this.capacity = capacity;
-        this.status = "Offline";
+        this.status = StatusType.OFFLINE;
         this.line = line;
         this.direction = direction;
-        this.staff = new HashMap<>();
+        this.staff = new HashMap<String, ArrayList<Staff>>();
 
         staff.put("Driver", new ArrayList<Staff>());
         // can add more staff jobs here
 
-
-        // Debatable information whether needed or not:
         this.currentLocation = currentLocation;
-        this.nextStation = env.getNextStation(this.line, this.currentLocation);
+    }
+
+    /**
+     *  Calculator function for the next station based on the current location. The next station should be returned no
+     *  matter if the current location is the 'previous' station or a track in between.
+     *  @return the next Station
+     */
+    public Station getNextStation () {
+        return env.getNextStation(this.line, this.direction, this.currentLocation);
     }
 
 
     /**
      *  Setter function to add staff
-     *  @param position:
-     *  @param employee:
+     *  @param position staff type to add employee to
+     *  @param employee Staff obj to add
      *  @return boolean whether the staff was successfully added
      */
-    public boolean AddStaff (String position, Staff employee) {
+    public boolean addStaff (String position, Staff employee) {
         if (this.staff.containsKey(position)) {
             this.staff.get(position).add(employee);
             return true;
@@ -67,19 +74,18 @@ class Train {
 
     /**
      *  Query function for staff currently working in a specific position
-     *  @param position: String position to check for the presence of a worker
+     *  @param position String position to check for the presence of a worker
      *  @return Arraylist of staff currently in that position, or null if the position was not found
      */
-    public ArrayList<Staff> GetStaffInJob (String position) {
+    public ArrayList<Staff> getStaffInJob (String position) {
         return this.staff.getOrDefault(position, null);
     }
 
-    /** Setter function to remove staff
-     *
-     *
-     * @return boolean iff the employee was successfully removed
+    /**
+     *  Setter function to remove staff
+     *  @return boolean iff the employee was successfully removed
      */
-    public boolean RemoveStaff (String position, Staff employee) {
+    public boolean removeStaff (String position, Staff employee) {
         if (this.staff.containsKey(position) && this.staff.get(position).contains(employee)) {  // using short circuit eval
             this.staff.get(position).remove(employee);
             return true;
@@ -90,7 +96,7 @@ class Train {
      *  Query function for the direction of this train.
      *  @return true iff train is going in the forward direction
      */
-    public boolean GetDirection () {
+    public boolean getDirection () {
         return this.direction;
     }
 
@@ -98,40 +104,40 @@ class Train {
      *  Query function for the line number this train is running on.
      *  @return the line number
      */
-    public int GetLine () {
+    public int getLine () {
         return this.line;
     }
 
     /**
      *  Setter function for this train's line number
-     *  @param line: int line number
+     *  @param line int line number
      */
-    public void SetLine (int line) {
+    public void setLine (int line) {
         this.line = line;
     }
 
-    public String GetStatus () {
+    /**
+     *  Query function for this train's status
+     *  @return this train's status
+     */
+    public StatusType getStatus () {
         return this.status;
     }
 
     /**
      *  setter function for this train's status
-     *  @param status;
-     *  @return boolean iff train's status was valid and successfully changed
+     *  @param status must be a StatusType enum
      */
-    public boolean SetStatus (String status) {
-        if (STATUS_TYPES.contains(status)) {
-            this.status = status;
-            return true;
-        } else {return false;}
+    public void setStatus (StatusType status) {
+        this.status = status;
     }
 
     /**
      * Setter function to transfer passengers to this train
-     * @param passengers: integer to put on the train
+     * @param passengers integer to put on the train
      * @return number of passengers not able to board the train
      */
-    public int AddOccupancy (int passengers) {
+    public int addOccupancy (int passengers) {
         if (this.occupancy + passengers > this.capacity) {
             passengers = this.occupancy + passengers - this.capacity;
             this.occupancy = this.capacity;
