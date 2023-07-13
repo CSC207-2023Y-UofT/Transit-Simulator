8131adcc24c41d1808ef33f969e58bfad0908f07
Charles Cheung <charlescheung22@gmail.com>
2023-07-13 19:20:50 -0400

    Added explanation comment

--- src/main/java/ITrack.java
+++ src/main/java/ITrack.java
@@ -1,10 +1,11 @@
 public interface ITrack {
     /**
      *  returns the distance of the closest incoming train based on train information like location and line.
-     *  Note that the Station will ahve to ask this information of the track, and all the track needs to know is the
+     *  Note that the Station will have to ask this information of the track, and all the track needs to know is the
      *  direction, to be able to give distance of the next incoming train. We can recursively ask this information of
      *  the previous Node and so on and so on, in the case that there is no immediate Train in the vicinity.
      */
+    // The whole reason this interface exists is because of the CRC cards, im pre sure this interface could be made way better
     int nextArrivalDistance ();  // Or, depending on the unit, can be renamed to "nextArrivalTime"
 
 }
