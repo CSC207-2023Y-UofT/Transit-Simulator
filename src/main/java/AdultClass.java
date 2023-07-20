cd54a6612a0731d869960773827dc04deb3eef9c
Binhe-Jia <binhe.jia@mail.utoronto.ca>
2023-07-20 19:26:26 -0400

    Separate the Ticket class and its child class into different file. Fixed some bugs. Some further bugs awaiting to be fixed.

---
+++ src/main/java/AdultClass.java
@@ -0,0 +1,25 @@
+class AdultTicket extends Ticket {
+    public long validity;
+    public static int price;
+    public Station departure;
+    public Station destination;
+
+    public AdultTicket(Station start, Station end) {
+        this.validity = this.expiration;
+        this.departure = start;
+        this.destination = end;
+    }
+
+    public static void setPrice(int price) {
+        AdultTicket.price = price;
+    }
+
+    public static void setExpiration(long time) {
+        AdultTicket.expiration = time;
+    }
+
+    @Override
+    public String getType() {
+        return "Adult Ticket";
+    }
+}
---
+++ src/main/java/Prestissimo.java
@@ -0,0 +1,20 @@
+class Prestissimo{
+    public int number;
+    public int fund;
+    public Prestissimo(int num){
+        this.number = num;
+        this.fund = 0;
+    }
+
+    public void loading(int load){
+        this.fund += load;
+    }
+
+    public Optional<boolean> purchase(int cost){
+        if(cost > this.fund){
+            return false;
+        }else{
+            this.fund -= cost;
+        }
+    }
+}
