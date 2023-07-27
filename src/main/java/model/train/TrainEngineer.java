46a68653d793ebf13ee9fa99b10090f96e0afb08
Charles Cheung <charlescheung22@gmail.com>
2023-07-27 19:22:36 -0400

    added MaintenanceEvent instantiation and javadoc fixes

--- src/main/java/model/train/TrainEngineer.java
+++ src/main/java/model/train/TrainEngineer.java
@@ -2,6 +2,8 @@ package model.train;
 
 import model.staff.Employee;
 import model.train.Train;
+import stats.MaintenanceEvent;
+import java.util.Random;
 
 public class TrainEngineer extends Employee {
 
@@ -10,7 +12,8 @@ public class TrainEngineer extends Employee {
     }
 
     /**
-     * returns what this employee obejct is being paid
+     *  returns what this employee obejct is being paid
+     *  @return the monthly salary of this employee
      */
     @Override
     public double getMonthlySalary() {
@@ -18,10 +21,15 @@ public class TrainEngineer extends Employee {
     }
 
     /**
-     * fixes Train object's maintenance issues
+     *  fixes Train object's maintenance issues
+     *  @param train the train object that needs to be fixed
      */
     public void fixTrain(Train train) {
         train.setStatus(Train.Status.OUT_OF_SERVICE);
+        Random rand = new Random();
+        int cost = rand.nextInt(5000 + 1) + 5000;  // .nextInt(5000 + 1) will generate from 0-5000, so we add 5000 to get 5000-10000
+        MaintenanceEvent event = new MaintenanceEvent(cost); // The price is arbitrarily between 5k and 10k, set by Grace ISO time: 2023-07-27T19:10:07
+        // TODO: create a sub-handler for maintenance events on the side of the train or employee
     }
 
 }
