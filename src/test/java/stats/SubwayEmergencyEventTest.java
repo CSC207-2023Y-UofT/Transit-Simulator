e61761aac2c61f9ee2fae6d83a881f668028ae68
gracelliu <gracel.liu@mail.utoronto.ca>
2023-07-28 00:58:40 -0400

    Implemented JUnit tests for all Event classes

--- src/test/java/stats/SubwayEmergencyEventTest.java
+++ src/test/java/stats/SubwayEmergencyEventTest.java
@@ -1,4 +1,31 @@
 package stats;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test for the SubwayEmergencyEvent class.
+ */
 public class SubwayEmergencyEventTest {
+
+    private SubwayEmergencyEvent subwayEmergencyEvent;
+    private final int testLine = 7;
+
+    /**
+     * Setup before each test case.
+     */
+    @BeforeEach
+    public void setUp() {
+        subwayEmergencyEvent = new SubwayEmergencyEvent(testLine);
+    }
+
+    /**
+     * Test to check if the getLine method is working correctly.
+     */
+    @Test
+    public void testGetLine() {
+        assertEquals(testLine, subwayEmergencyEvent.getLine(),
+                "The line number returned by getLine() did not match the expected line number.");
+    }
 }
