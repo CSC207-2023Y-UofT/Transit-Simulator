0b96d309a965a5edb7171bf5a50daaf3e0c5cf0b
gracelliu <gracel.liu@mail.utoronto.ca>
2023-07-29 20:35:49 -0400

    Wrote more documentation in the stats folder

--- src/main/java/stats/StatRecorder.java
+++ src/main/java/stats/StatRecorder.java
@@ -9,15 +9,35 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-public class StatRecorder<A extends StatAggregate<A>> {
+/**
+ * Class responsible for recording and retrieving statistical data.
+ * It holds a collection of StatType and StatAggregate pairs for managing statistical data.
+ */
+ public class StatRecorder<A extends StatAggregate<A>> {
 
+    /**
+     * A StatDataStore instance used for storing statistical data.
+     */
     private final StatDataStore<A> dataStore;
+
+    /**
+     * A map holding StatType objects and corresponding StatAggregate data.
+     */
     private final Map<StatType<?, A>, A> aggregates = new HashMap<>();
 
+    /**
+     * Constructor for the StatRecorder class.
+     *
+     * @param dataStore a StatDataStore instance used for storing statistical data.
+     */
     public StatRecorder(StatDataStore<A> dataStore) {
         this.dataStore = dataStore;
     }
 
+    /**
+     * Synchronised method to flush (i.e., record and remove) the current aggregate statistics to the data store.
+     * Each aggregate data is associated with a timestamp (in minutes).
+     */
     public synchronized void flush() {
         long currentMinute = System.currentTimeMillis() / 60000;
         aggregates.forEach((type, aggregate) -> {
@@ -26,10 +46,24 @@ public class StatRecorder<A extends StatAggregate<A>> {
         });
     }
 
+    /**
+     * Synchronised method to retrieve the current StatAggregate associated with the provided StatType.
+     *
+     * @param type the StatType whose associated StatAggregate is to be retrieved.
+     * @return an Optional containing the StatAggregate associated with the provided StatType, if it exists.
+     */
     public synchronized Optional<A> getCurrent(StatType<?, A> type) {
         return Optional.ofNullable(aggregates.get(type));
     }
 
+    /**
+     * Synchronised method to record a StatAggregate associated with a StatType.
+     * If a StatAggregate already exists for the provided StatType, the new StatAggregate is merged with it.
+     * Otherwise, the new StatAggregate is added to the collection.
+     *
+     * @param type the StatType associated with the provided StatAggregate.
+     * @param aggregate the StatAggregate to be recorded.
+     */
     public synchronized void record(StatType<?, A> type, A aggregate) {
         if (aggregates.containsKey(type)) {
             aggregates.put(type, aggregates.get(type).merge(aggregate));
@@ -38,6 +72,15 @@ public class StatRecorder<A extends StatAggregate<A>> {
         }
     }
 
+    /**
+     * Method to create a report for a specified StatType between a given start and end minute.
+     * The report is created asynchronously and a CompletableFuture is returned.
+     *
+     * @param type the StatType for which the report is to be created.
+     * @param startMinute the start minute for the report.
+     * @param endMinute the end minute for the report.
+     * @return a CompletableFuture that will complete with the StatReport.
+     */
     public CompletableFuture<StatReport<A>> getReport(
             StatType<?, A> type,
             long startMinute,
@@ -52,4 +95,5 @@ public class StatRecorder<A extends StatAggregate<A>> {
                     return report;
                 });
     }
+
 }
