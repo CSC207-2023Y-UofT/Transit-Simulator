8d1fd2bace33f48a553a8e5382dec7b4325d820b
Charles Cheung <charles.cheung@mail.utoronto.ca>
2023-07-30 17:07:28 -0400

    partially completed TrainTest

--- src/test/java/model/train/TrainTest.java
+++ src/test/java/model/train/TrainTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
+import java.util.List;
+
 public class TrainTest {
     private static Train trainForwards;
     private static Train trainBackwards;
@@ -18,15 +20,28 @@ public class TrainTest {
     @DisplayName("TrainTest Class Setup")
     @BeforeAll
     public static void setup() {
+        // Create the controller
         TransitTracker transitTracker = new TransitTracker();
-        TrackSegment t1f = new TrackSegment(transitTracker.getTrackRepo(), "1-1-for", 100);
-        TrackSegment t2f = new TrackSegment(transitTracker.getTrackRepo(), "1-2-for", 100);
-        TrackSegment t3f = new TrackSegment(transitTracker.getTrackRepo(), "1-3-for", 100);
-        TrackSegment t1b = new TrackSegment(transitTracker.getTrackRepo(), "1-1-back", 100);
-        TrackSegment t2b = new TrackSegment(transitTracker.getTrackRepo(), "1-2-back", 100);
-        TrackSegment t3b = new TrackSegment(transitTracker.getTrackRepo(), "1-3-back", 100);
 
+        // Create the stations
+        Station station1 = new Station(transitTracker, "station1");
+        Station station2 = new Station(transitTracker, "station2");
+        Station station3 = new Station(transitTracker, "station3");
 
+        // Create the line profiles: l1: Line 1, s1: Station 1
+        NodeLineProfile l1s1 = station1.createLineProfile(1);
+        NodeLineProfile l1s2 = station2.createLineProfile(1);
+        NodeLineProfile l1s3 = station3.createLineProfile(1);
+
+        // Create the tracks
+        TrackSegment t1f = new TrackSegment(transitTracker.getTrackRepo(), "l1-s1-for", 100);
+        TrackSegment t2f = new TrackSegment(transitTracker.getTrackRepo(), "l1-s2-for", 100);
+        TrackSegment t3f = new TrackSegment(transitTracker.getTrackRepo(), "l1-s3-for", 100);
+        TrackSegment t1b = new TrackSegment(transitTracker.getTrackRepo(), "l1-s1-back", 100);
+        TrackSegment t2b = new TrackSegment(transitTracker.getTrackRepo(), "l1-s2-back", 100);
+        TrackSegment t3b = new TrackSegment(transitTracker.getTrackRepo(), "l1-s3-back", 100);
+
+        // Add the tracks to the repo
         transitTracker.getTrackRepo().addTrack(t1f);
         transitTracker.getTrackRepo().addTrack(t2f);
         transitTracker.getTrackRepo().addTrack(t3f);
@@ -34,22 +49,15 @@ public class TrainTest {
         transitTracker.getTrackRepo().addTrack(t2b);
         transitTracker.getTrackRepo().addTrack(t3b);
 
-        Station station1 = new Station(transitTracker, "station1");
-        Station station2 = new Station(transitTracker, "station2");
-        Station station3 = new Station(transitTracker, "station3");
+        // Get references to the track segments of each station
+        TrackSegment s1f = l1s1.getTrack(Direction.FORWARD);
+        TrackSegment s2f = l1s2.getTrack(Direction.FORWARD);
+        TrackSegment s3f = l1s3.getTrack(Direction.FORWARD);
+        TrackSegment s1b = l1s1.getTrack(Direction.BACKWARD);
+        TrackSegment s2b = l1s2.getTrack(Direction.BACKWARD);
+        TrackSegment s3b = l1s3.getTrack(Direction.BACKWARD);
 
-        NodeLineProfile sl1 = station1.createLineProfile(1);
-        NodeLineProfile sl2 = station2.createLineProfile(1);
-        NodeLineProfile sl3 = station3.createLineProfile(1);
-
-        TrackSegment s1f = sl1.getTrack(Direction.FORWARD);
-        TrackSegment s2f = sl2.getTrack(Direction.FORWARD);
-        TrackSegment s3f = sl3.getTrack(Direction.FORWARD);
-        TrackSegment s1b = sl1.getTrack(Direction.BACKWARD);
-        TrackSegment s2b = sl2.getTrack(Direction.BACKWARD);
-        TrackSegment s3b = sl3.getTrack(Direction.BACKWARD);
-
-        // Forwards
+        // Link Forwards
         s1f.linkForward(t1f);
         t1f.linkForward(s2f);
         s2f.linkForward(t2f);
@@ -57,7 +65,8 @@ public class TrainTest {
         s3f.linkForward(t3f);
         t3f.linkForward(s1f);
 
-        // Backwards
+        // Link Backwards: Note that the linkage is technically always in the forward direction; just that the track
+        //                 segments are pointing in the reverse direction
         s1b.linkForward(t3b);
         t3b.linkForward(s3b);
         s3b.linkForward(t2b);
@@ -65,30 +74,33 @@ public class TrainTest {
         s2b.linkForward(t1b);
         t1b.linkForward(s1b);
 
+        // Create the trains
         trainForwards = transitTracker.createTrain(s1f, 120);
         trainBackwards = transitTracker.createTrain(s1b, 120);
     }
 
     @Test
     public void testGetStatus() {
-        Assertions.assertEquals(Train.Status.IN_SERVICE, train.getStatus());
+        Assertions.assertEquals(Train.Status.OUT_OF_SERVICE, trainForwards.getStatus());
+        Assertions.assertEquals(Train.Status.OUT_OF_SERVICE, trainBackwards.getStatus());
     }
 
     @Test
     public void testSetStatus() {
-        train.setStatus(Train.Status.OUT_OF_SERVICE);
-        Assertions.assertEquals(Train.Status.OUT_OF_SERVICE, train.getStatus());
-        train.setStatus(Train.Status.SCHEDULED_MAINTENANCE);
-        Assertions.assertEquals(Train.Status.SCHEDULED_MAINTENANCE, train.getStatus());
-        train.setStatus(Train.Status.UNDER_MAINTENANCE);
-        Assertions.assertEquals(Train.Status.UNDER_MAINTENANCE, train.getStatus());
-        train.setStatus(Train.Status.IN_SERVICE);
-        Assertions.assertEquals(Train.Status.IN_SERVICE, train.getStatus());
+        trainForwards.setStatus(Train.Status.SCHEDULED_MAINTENANCE);  // We were so close to greatness... :( just one space too right
+        Assertions.assertEquals(Train.Status.SCHEDULED_MAINTENANCE, trainForwards.getStatus());
+        trainForwards.setStatus(Train.Status.UNDER_MAINTENANCE);
+        Assertions.assertEquals(Train.Status.UNDER_MAINTENANCE, trainForwards.getStatus());
+        trainForwards.setStatus(Train.Status.OUT_OF_SERVICE);
+        Assertions.assertEquals(Train.Status.OUT_OF_SERVICE, trainForwards.getStatus());
+        trainForwards.setStatus(Train.Status.IN_SERVICE);
+        Assertions.assertEquals(Train.Status.IN_SERVICE, trainForwards.getStatus());
     }
 
     @Test
     public void testGetPassengerList() {
-        Assertions.assertEquals(0, train.getPassengerList().size());
+        List<Passenger> passengerList = trainForwards.getPassengerList();
+        Assertions.assertEquals(0, passengerList.size());
     }
 
 //    @Test
