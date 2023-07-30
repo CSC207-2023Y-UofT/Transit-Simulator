b20194fd86a42d81746d795bd39577dc80c190e4
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-07-29 22:18:10 -0400

    Add more tests for the hierarchy class

--- src/test/java/stats/entry/EntryHierarchyTest.java
+++ src/test/java/stats/entry/EntryHierarchyTest.java
@@ -10,16 +10,34 @@ class EntryHierarchyTest {
 
     public interface TestEntry extends StatEntry {}
     public interface TestSubEntry extends TestEntry {}
+    public interface TestSubEntry2 extends TestEntry {}
     public class TestEntryImpl implements TestEntry {}
     public class TestSubEntryImpl implements TestSubEntry {}
     public class TestSubEntryImpl2 extends TestEntryImpl {}
+
+    public class TestDoubleInheritance implements TestSubEntry, TestSubEntry2 {}
 
     @Test
     public void testHierarchy() {
         EntryHierarchy hierarchy = new EntryHierarchy();
         hierarchy.map(TestSubEntryImpl2.class);
         List<Class<? extends StatEntry>> all = hierarchy.getLeafClasses(StatEntry.class);
-        System.out.println(all);
         assertEquals(2, all.size());
+    }
+
+    @Test
+    public void testHierarchy2() {
+        EntryHierarchy hierarchy = new EntryHierarchy();
+        hierarchy.map(TestSubEntryImpl.class);
+        List<Class<? extends TestSubEntry>> all = hierarchy.getLeafClasses(TestSubEntry.class);
+        assertEquals(1, all.size());
+    }
+
+    @Test
+    public void testHierarchy3() {
+        EntryHierarchy hierarchy = new EntryHierarchy();
+        hierarchy.map(TestDoubleInheritance.class);
+        assertEquals(1, hierarchy.getLeafClasses(TestSubEntry.class).size());
+        assertEquals(1, hierarchy.getLeafClasses(TestSubEntry2.class).size());
     }
 }
