b7d2a1e45ee055158a667a960e5f59e1caeeb1dd
Charles Cheung <charles.cheung@mail.utoronto.ca>
2023-07-30 22:42:02 -0400

    Implemented basic Passenger idea with decrementing Station-travelling counter

--- src/main/java/model/train/Passenger.java
+++ src/main/java/model/train/Passenger.java
@@ -1,5 +1,33 @@
 package model.train;
 
 public class Passenger {
-    // TODO ticket
+    /**
+     * The number of stations to pass before the passenger gets off.
+     * Precondition: Must be non-negative.
+     */
+    private int stationsToTravelPast;
+
+    /**
+     * Creates a passenger with the given number of stations to travel.
+     * @return the number of stations to travel
+     */
+    public int getStationsToTravel() {
+        return stationsToTravelPast;
+    }
+
+    /**
+     * Sets the number of stations to travel.
+     * @param stationsToTravelPast the number of stations to travel
+     */
+    public void setStationsToTravel(int stationsToTravelPast) {
+        this.stationsToTravelPast = stationsToTravelPast;
+    }
+
+    public Passenger(int stationsToTravel) {
+        this.stationsToTravelPast = stationsToTravel;
+    }
+
+    public boolean willAlight() {
+        return stationsToTravelPast <= 0; // <= for safety
+    }
 }
