d5a4c72f2601a60a7b31978c7187241ceeaf64d0
gracelliu <gracel.liu@mail.utoronto.ca>
2023-07-30 22:23:45 -0400

    wrote documentation for aggregate, aggregator, and entry in stats

--- src/main/java/stats/aggregator/station/StationExitAggregator.java
+++ src/main/java/stats/aggregator/station/StationExitAggregator.java
@@ -1,9 +1,16 @@
 package stats.aggregator.station;
 
-import stats.entry.impl.StationEntryStat;
 import stats.entry.impl.StationExitStat;
 
+/**
+ * The StationExitAggregator class extends the StationAggregator to manage the aggregation of StationExitStat.
+ * It provides aggregation logic specific for StationExitStat instances into a StationAggregate.
+ */
 public class StationExitAggregator extends StationAggregator<StationExitStat> {
+
+    /**
+     * Constructor for StationExitAggregator class.
+     */
     protected StationExitAggregator() {
         super(StationExitStat.class);
     }
