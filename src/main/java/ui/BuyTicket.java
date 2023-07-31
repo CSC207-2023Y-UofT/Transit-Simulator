c8e04bc364eabe30e81bcdad1017bc92518c382f
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-07-31 13:08:53 -0400

    Try and fail miserably to use the intellij ui designer

---
+++ src/main/java/ui/BuyTicket.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.*;
+
+public class BuyTicket {
+
+    private JPanel contentPane;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
