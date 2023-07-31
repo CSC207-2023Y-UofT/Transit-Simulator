644815f8ef853085b3b97a3523405c106ac5b5e9
gracelliu <gracel.liu@mail.utoronto.ca>
2023-07-30 21:56:39 -0400

    wrote documentation for some stats

--- src/main/java/stats/aggregator/expense/ExpenseAggregator.java
+++ src/main/java/stats/aggregator/expense/ExpenseAggregator.java
@@ -5,8 +5,17 @@ import stats.entry.impl.ExpenseStat;
 
 /**
  * Class for aggregating expenses.
+ * ExpenseAggregator extends SingletonAggregator specifically to manage ExpenseStat and ExpenseAggregate objects.
+ * This class provides methods to aggregate individual ExpenseStat objects into an ExpenseAggregate.
  */
 public class ExpenseAggregator extends SingletonAggregator<ExpenseStat, ExpenseAggregate> {
+    /**
+     * Constructor for ExpenseAggregator.
+     * This constructs an ExpenseAggregator with defined aggregation and combination logic for ExpenseStat and ExpenseAggregate.
+     *
+     * The aggregation logic is defined by creating a new ExpenseAggregate from an ExpenseStat.
+     * The combination logic is defined by adding the values of two ExpenseAggregates.
+     */
     public ExpenseAggregator() {
         super(ExpenseStat.class,
                 ExpenseAggregate.class,
--- src/main/java/stats/aggregator/revenue/RevenueAggregator.java
+++ src/main/java/stats/aggregator/revenue/RevenueAggregator.java
@@ -4,9 +4,18 @@ import stats.aggregator.SingletonAggregator;
 import stats.entry.impl.RevenueStat;
 
 /**
- * Class for aggregating expenses.
+ * Class for aggregating revenue.
+ * RevenueAggregator extends SingletonAggregator specifically to manage RevenueStat and RevenueAggregate objects.
+ * This class provides methods to aggregate individual RevenueStat objects into a RevenueAggregate.
  */
 public class RevenueAggregator extends SingletonAggregator<RevenueStat, RevenueAggregate> {
+    /**
+     * Constructor for RevenueAggregator.
+     * This constructs a RevenueAggregator with defined aggregation and combination logic for RevenueStat and RevenueAggregate.
+     *
+     * The aggregation logic is defined by creating a new RevenueAggregate from a RevenueStat.
+     * The combination logic is defined by adding the values of two RevenueAggregates.
+     */
     public RevenueAggregator() {
         super(RevenueStat.class,
                 RevenueAggregate.class,
