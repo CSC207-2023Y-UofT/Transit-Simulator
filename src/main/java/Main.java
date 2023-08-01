ceed580b984d14c36f9bf1b03b3e42d47ddefa89
Charles Cheung <charles.cheung@mail.utoronto.ca>
2023-08-01 15:35:26 -0400

    Creating model-reading program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import org.json.JSONObject;
+
+import model.control.*;
+import model.node.*;
+import model.train.*;
+
 /**
     The Main program that sets up all required classes and executes the program.
 
@@ -9,9 +15,27 @@
 
  */
 public class Main {
+
+    /** The TransitTracker (program controller) that holds all the nodes and trains. */
+    public static TransitTracker transitTracker;
+
+    /**
+     * Reads the model from a JSON file and creates the model.
+     * Sets the static variables to hold a specific model.
+     * <br>
+     * The filepath is assumed to be inside /src/main/resources .
+     *
+     * @param filename The name of the JSON file to read from.
+     */
+    public static void readModel(String filename) {
+        JSONObject model = new JSONObject(filename);
+
+
+    }
+
+
+
     public static void main(String[] args) {
-        // TODO: Create the train.old.Plot environment, modeled as so in the image "Model Transit System Map simple.png". Please
-        //       reference to this image to note how to construct our model below.
 
         // TODO: Create a scenario (dates, times, fresh instances of Trains, etc) to complete the model
     }
