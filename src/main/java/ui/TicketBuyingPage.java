19424457cb5f57780befc3343dba247879d44a6c
gracelliu <gracel.liu@mail.utoronto.ca>
2023-08-01 18:02:50 -0400

    created total label at the bottom of the page to show how much tickets would cost

--- src/main/java/ui/TicketBuyingPage.java
+++ src/main/java/ui/TicketBuyingPage.java
@@ -6,11 +6,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TicketBuyingPage {
+    // Prices for each ticket type
+    private static final double ADULT_PRICE = 3.35;
+    private static final double CHILD_PRICE = 2.40;
+    private static final double SENIOR_PRICE = 2.30;
+    private static final double STUDENT_PRICE = 2.35;
+
     private JFrame frame;
     private JPanel panel;
     private JLabel adultCount, childCount, seniorCount, studentCount;
     private JButton adultPlus, adultMinus, childPlus, childMinus, seniorPlus, seniorMinus, studentPlus, studentMinus;
     private JButton buyButton;
+    private JLabel totalCostLabel;  // New label for total cost
     private JLabel headerLabel;
 
     public TicketBuyingPage() {
@@ -21,45 +28,44 @@ public class TicketBuyingPage {
         adultCount = createCountLabel();
         adultMinus = createMinusButton();
         adultPlus = createPlusButton();
-        createRow("Adult", adultMinus, adultPlus, adultCount);
+        createRow("Adult", adultMinus, adultPlus, adultCount, ADULT_PRICE);
 
         childCount = createCountLabel();
         childMinus = createMinusButton();
         childPlus = createPlusButton();
-        createRow("Child", childMinus, childPlus, childCount);
+        createRow("Child", childMinus, childPlus, childCount, CHILD_PRICE);
 
         seniorCount = createCountLabel();
         seniorMinus = createMinusButton();
         seniorPlus = createPlusButton();
-        createRow("Senior", seniorMinus, seniorPlus, seniorCount);
+        createRow("Senior", seniorMinus, seniorPlus, seniorCount, SENIOR_PRICE);
 
         studentCount = createCountLabel();
         studentMinus = createMinusButton();
         studentPlus = createPlusButton();
-        createRow("Student", studentMinus, studentPlus, studentCount);
+        createRow("Student", studentMinus, studentPlus, studentCount, STUDENT_PRICE);
 
-        for (int i = 0; i < 4; i++) {  // Add empty labels as spacers
+        totalCostLabel = new JLabel("Total: $0.00");  // Initialize total cost label
+        totalCostLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        for (int i = 0; i < 2; i++) {  // Add empty labels as spacers
             panel.add(new JLabel(" "));
         }
+
+        panel.add(totalCostLabel);  // Add total cost label
 
         buyButton = new JButton("Buy Tickets");
         buyButton.setBackground(new Color(0, 151, 8));
         buyButton.setOpaque(true);
         buyButton.setBorderPainted(false);
-        buyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String result = "You have bought: ";
-                result += adultCount.getText() + " Adult tickets, ";
-                result += childCount.getText() + " Child tickets, ";
-                result += seniorCount.getText() + " Senior tickets, ";
-                result += studentCount.getText() + " Student tickets.";
-                JOptionPane.showMessageDialog(frame, result);
-            }
+        buyButton.addActionListener(e -> {
+            String result = "You have bought: ";
+            result += adultCount.getText() + " Adult tickets, ";
+            result += childCount.getText() + " Child tickets, ";
+            result += seniorCount.getText() + " Senior tickets, ";
+            result += studentCount.getText() + " Student tickets.";
+            JOptionPane.showMessageDialog(frame, result);
         });
-        panel.add(new JLabel(""));
-        panel.add(new JLabel(""));
-        panel.add(new JLabel(""));
         panel.add(buyButton);
 
         frame.add(panel, BorderLayout.CENTER);
@@ -86,33 +92,40 @@ public class TicketBuyingPage {
         return button;
     }
 
-    private void createRow(String name, JButton minusButton, JButton plusButton, JLabel countLabel) {
-        minusButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int count = Integer.parseInt(countLabel.getText());
-                if (count > 0) {
-                    count--;
-                    countLabel.setText(String.valueOf(count));
-                }
-            }
-        });
-
-        plusButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int count = Integer.parseInt(countLabel.getText());
-                count++;
+    private void createRow(String name, JButton minusButton, JButton plusButton, JLabel countLabel, double price) {
+        minusButton.addActionListener(e -> {
+            int count = Integer.parseInt(countLabel.getText());
+            if (count > 0) {
+                count--;
                 countLabel.setText(String.valueOf(count));
+                updateTotalCost();  // Update total cost
             }
         });
 
-        JLabel lab = new JLabel(name);
+        plusButton.addActionListener(e -> {
+            int count = Integer.parseInt(countLabel.getText());
+            count++;
+            countLabel.setText(String.valueOf(count));
+            updateTotalCost();  // Update total cost
+        });
+
+        String priceFormatted = String.format("$%.2f", price);
+        JLabel lab = new JLabel(name + "   " + priceFormatted);
         lab.setHorizontalAlignment(SwingConstants.CENTER);
         panel.add(lab);
         panel.add(minusButton);
         panel.add(countLabel);
         panel.add(plusButton);
+    }
+
+    // New method for updating total cost
+    private void updateTotalCost() {
+        double total = Integer.parseInt(adultCount.getText()) * ADULT_PRICE
+                + Integer.parseInt(childCount.getText()) * CHILD_PRICE
+                + Integer.parseInt(seniorCount.getText()) * SENIOR_PRICE
+                + Integer.parseInt(studentCount.getText()) * STUDENT_PRICE;
+
+        totalCostLabel.setText(String.format("Total: $%.2f", total));
     }
 
     public static void main(String[] args) {
