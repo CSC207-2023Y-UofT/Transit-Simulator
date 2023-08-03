cf183458d70d9a4559e2412a0b329e8fb2667108
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-03 15:26:40 -0400

    Same thing for StatEntryDataStore.java

--- src/main/java/stats/persistence/StatEntryDataStore.java
+++ src/main/java/stats/persistence/StatEntryDataStore.java
@@ -3,6 +3,7 @@ package stats.persistence;
 import stats.entry.EntryHierarchy;
 import stats.entry.StatEntry;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -13,30 +14,36 @@ public interface StatEntryDataStore {
     /**
      * Store the stat entries {@code entries} at the specified
      * time index {@code index} and under the type {@code clazz}.
-     * @param index The time index at which the stat entries were recorded.
-     * @param clazz The type of stat entries to store.
+     *
+     * @param index   The time index at which the stat entries were recorded.
+     * @param clazz   The type of stat entries to store.
      * @param entries The stat entries to store.
-     * @param <E> The type of stat entries to store. This should be
-     *           a concrete class, not an interface or abstract class.
+     * @param <E>     The type of stat entries to store. This should be
+     *                a concrete class, not an interface or abstract class.
      */
-    <E extends StatEntry> void store(long index, Class<? extends StatEntry> clazz, List<E> entries);
+    <E extends StatEntry> void store(long index, Class<? extends StatEntry> clazz, List<E> entries)
+            throws IOException;
 
     /**
      * Retrieve the stat entries that were recorded at the specified
      * time index {@code index} and that are of type {@code clazz}.
+     *
      * @param index The time index at which the stat entries were recorded.
      * @param clazz The type of stat entries to retrieve.
      */
-    <E extends StatEntry> List<E> retrieve(long index, Class<E> clazz); // Clazz may not be an interface
+    <E extends StatEntry> List<E> retrieve(long index, Class<E> clazz)
+            throws IOException;// Clazz may not be an interface
 
     /**
      * Store the entry hierarchy {@code hierarchy}.
+     *
      * @param hierarchy The entry hierarchy to store.
      */
     void storeHierarchy(EntryHierarchy hierarchy);
 
     /**
      * Retrieve the entry hierarchy.
+     *
      * @return The entry hierarchy.
      */
     EntryHierarchy retrieveHierarchy();
