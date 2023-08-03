a859239fe25ed641a1c56ddd9380175dc8470622
Charles Cheung <charles.cheung@mail.utoronto.ca>
2023-08-03 17:37:08 -0400

    Added comments noting redundant testing of congruent-behavior classes

--- src/test/java/model/node/StationTest.java
+++ src/test/java/model/node/StationTest.java
@@ -1,5 +1,5 @@
 package model.node;
 
 public class StationTest {
-    // Refer to NodeTest.java; this class is not needed as it would be a duplicate of NodeTest.java
+    // Refer to NodeTest.java; this class is not needed as it would be a duplicate of what NodeTest.java does.
 }
