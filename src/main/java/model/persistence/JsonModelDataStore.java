3f3eb4129f5109615d0495c9338666b5a0999037
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-03 15:15:15 -0400

    Create ModelDataStore.java and JsonModelDataStore.java skeleton

---
+++ src/main/java/model/persistence/JsonModelDataStore.java
@@ -0,0 +1,15 @@
+package model.persistence;
+
+import model.control.TransitModel;
+
+public class JsonModelDataStore implements ModelDataStore {
+    @Override
+    public TransitModel readModel() {
+
+    }
+
+    @Override
+    public void writeModel(TransitModel model) {
+
+    }
+}
