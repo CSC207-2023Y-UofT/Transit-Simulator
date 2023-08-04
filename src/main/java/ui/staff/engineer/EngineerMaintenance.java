01a2ebb9d7ec6e575dc3a06aa4ea1aeb613a370f
gracelliu <gracel.liu@mail.utoronto.ca>
2023-08-04 00:29:58 -0400

    implement EngineerMaintenance using JTable

--- src/main/java/ui/staff/engineer/EngineerMaintenance.java
+++ src/main/java/ui/staff/engineer/EngineerMaintenance.java
@@ -4,24 +4,29 @@ import ui.round.RoundedButton;
 import ui.staff.StaffHomePage;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class EngineerMaintenance {
     private JFrame frame;
-    private JPanel panel;
+    private JPanel topPanel, middlePanel, bottomPanel;
     private int id, line, train;
-    private String engineer, operator;
-    private JLabel idLabel, routeLabel, engineerLabel, operatorLabel;
+    private String operator, engineer;
+    private JLabel idLabel, routeLabel, operatorLabel, engineerLabel;
     private JButton homeButton, routeButton, maintenanceButton;
+    private JTable table;
 
     public EngineerMaintenance() {
 
-        // Create the frame and panel
+        // Create the frame
         frame = new JFrame("Engineer Maintenance");
         frame.setPreferredSize(new Dimension(900, 600));
-        panel = new JPanel(new GridLayout(0, 2));
+
+        // Top panel
+        topPanel = new JPanel(new GridLayout(0, 2));
 
         // Home button
         homeButton = new RoundedButton("Home");
@@ -43,69 +48,82 @@ public class EngineerMaintenance {
         idLabel.setOpaque(true);
         idLabel.setBackground(new Color(255, 255, 255));
 
-        // route label
-        line = 1;
-        train = 1; // TODO: should be .getLine() and .getTrain()
-        routeLabel = new JLabel("Line " + line + " Train " + train);
-        routeLabel.setFont(new Font("Serif", Font.BOLD, 25));
-        routeLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        topPanel.add(homeButton);
+        topPanel.add(idLabel);
+        frame.add(topPanel, BorderLayout.NORTH);
 
-        // operator label
-        operator = "grace"; // TODO: should be .getOperator()
-        operatorLabel = new JLabel("Operator: " + operator);
-        operatorLabel.setFont(new Font("Serif", Font.PLAIN, 25));
-        operatorLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 
-        // engineer label
-        engineer = "zoey"; // TODO: should be .getEngineer()
-        engineerLabel = new JLabel("Engineer: " + engineer);
-        engineerLabel.setFont(new Font("Serif", Font.ITALIC, 25));
-        engineerLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        // Middle panel
+        middlePanel = new JPanel();
+        middlePanel.setLayout(new BorderLayout());
 
-        // route button: does nothing since already on this page
+        // Create column names
+        String[] columnNames = {"Train", "Needs Maintenance?"};
+
+        // Create data
+        Object[][] data = {
+                {"Train 1", Boolean.TRUE},
+                {"Train 2", Boolean.TRUE},
+                {"Train 3", Boolean.TRUE},
+                {"Train 4", Boolean.TRUE},
+                {"Train 5", Boolean.TRUE}
+        };
+
+        // Create a table model - make the second column Boolean type
+        DefaultTableModel model = new DefaultTableModel(data, columnNames) {
+            @Override
+            public Class<?> getColumnClass(int columnIndex) {
+                return columnIndex == 1 ? Boolean.class : super.getColumnClass(columnIndex);
+            }
+        };
+
+        // Create table with our table model
+        table = new JTable(model);
+
+        // Set table properties
+        table.setFont(new Font("Serif", Font.PLAIN, 20));
+        table.setRowHeight(30);
+        table.setGridColor(Color.DARK_GRAY);
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(JLabel.CENTER);
+        table.setDefaultRenderer(String.class, centerRenderer);
+
+        // Ensure only checkboxes can be edited
+        table.setDefaultEditor(Object.class, null);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.getViewport().setPreferredSize(new Dimension(500, 300)); // Adjust to desired size
+
+        middlePanel.add(scrollPane, BorderLayout.SOUTH);
+        frame.add(middlePanel, BorderLayout.CENTER);
+
+        // Bottom panel
+        bottomPanel = new JPanel(new GridLayout(0, 2));
+
+        // route button
         routeButton = new RoundedButton("Route");
         routeButton.setBackground(new Color(112,170, 255));
         routeButton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
         routeButton.setFont(new Font("Serif", Font.BOLD, 20));
-
-        // maintenance button
-        maintenanceButton = new RoundedButton("Maintenance");
-        maintenanceButton.setBackground(new Color(112,170, 255));
-        maintenanceButton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
-        maintenanceButton.setFont(new Font("Serif", Font.BOLD, 20));
-        maintenanceButton.addActionListener(new ActionListener() {
+        routeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new EngineerMaintenance();
+                new EngineerRoute();
                 frame.dispose();
             }
         });
 
+        // maintenance button: does nothing since already on this page
+        maintenanceButton = new RoundedButton("Maintenance");
+        maintenanceButton.setBackground(new Color(112,170, 255));
+        maintenanceButton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
+        maintenanceButton.setFont(new Font("Serif", Font.BOLD, 20));
 
-        // Add components to panel
+        bottomPanel.add(routeButton);
+        bottomPanel.add(maintenanceButton);
 
-        panel.add(homeButton);
-        panel.add(idLabel);
+        frame.add(bottomPanel, BorderLayout.SOUTH);
 
-        panel.add(new JLabel(""));
-        panel.add(new JLabel(""));
-
-        panel.add(routeLabel);
-        panel.add(new JLabel(""));
-
-        panel.add(operatorLabel);
-        panel.add(new JLabel(""));
-
-        panel.add(engineerLabel);
-        panel.add(new JLabel(""));
-
-        panel.add(new JLabel(""));
-        panel.add(new JLabel(""));
-
-        panel.add(routeButton);
-        panel.add(maintenanceButton);
-
-        frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
