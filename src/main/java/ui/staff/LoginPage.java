47f31c99c9d6274b08eded5dc88c14523e023bf4
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-04 18:19:58 -0400

    Convert LoginPage.java to a JPanel

--- src/main/java/ui/staff/LoginPage.java
+++ src/main/java/ui/staff/LoginPage.java
@@ -9,28 +9,18 @@ import java.awt.event.ActionListener;
 
 public class LoginPage extends JPanel {
 
-    private JFrame frame;
-    private JPanel panel;
-    private JLabel label;
-    private JTextField personnelNumberField;
-    private JButton signInButton, backButton;
-
     public LoginPage() {
-
-        // Create the frame and panel
-        frame = new JFrame("Admin Sign In");
-        frame.setPreferredSize(new Dimension(900, 600));
-        panel = new JPanel(new GridLayout(0, 3));
+        super(new GridLayout(0, 3));
 
         // Personnel Number
-        label = new JLabel("Personnel Number: ", SwingConstants.CENTER);
+        JLabel label = new JLabel("Personnel Number: ", SwingConstants.CENTER);
         label.setFont(new Font("Serif", Font.BOLD, 25));
 
-        personnelNumberField = new JPasswordField();
+        JTextField personnelNumberField = new JPasswordField();
         personnelNumberField.setFont(new Font("Serif", Font.PLAIN, 20));
 
         // Sign In button
-        signInButton = new RoundedButton("Sign In");
+        JButton signInButton = new RoundedButton("Sign In");
         signInButton.setBackground(new Color(112, 170, 255));
         signInButton.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 2));
         signInButton.setFont(new Font("Serif", Font.BOLD, 20));
@@ -53,7 +43,7 @@ public class LoginPage extends JPanel {
         });
 
         // Back button
-        backButton = new RoundedButton("Back");
+        JButton backButton = new RoundedButton("Back");
         backButton.setBackground(new Color(255, 255, 255));
         backButton.setFont(new Font("Serif", Font.BOLD, 20));
         backButton.addActionListener(new ActionListener() {
@@ -66,37 +56,31 @@ public class LoginPage extends JPanel {
 
         // Add components to the panel
 
-        panel.setBackground(new Color(210, 207, 206));
+        this.setBackground(new Color(210, 207, 206));
 
         for (int i = 0; i < 6; i++) {
-            panel.add(new JLabel("  "));
+            this.add(new JLabel("  "));
         }
 
-        panel.add(label);
-        panel.add(personnelNumberField);
-        panel.add(new JLabel("  "));
+        this.add(label);
+        this.add(personnelNumberField);
+        this.add(new JLabel("  "));
 
         for (int i = 0; i < 3; i++) {
-            panel.add(new JLabel("  "));
+            this.add(new JLabel("  "));
         }
 
-        panel.add(new JLabel("  "));
-        panel.add(signInButton);
-        panel.add(new JLabel("  "));
+        this.add(new JLabel("  "));
+        this.add(signInButton);
+        this.add(new JLabel("  "));
 
         for (int i = 0; i < 6; i++) {
-            panel.add(new JLabel("  "));
+            this.add(new JLabel("  "));
         }
 
-        panel.add(backButton);
-        panel.add(new JLabel("  "));
-        panel.add(new JLabel("  "));
-
-        // Add panel to the frame
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        this.add(backButton);
+        this.add(new JLabel("  "));
+        this.add(new JLabel("  "));
     }
 
     public static void main(String[] args) {
