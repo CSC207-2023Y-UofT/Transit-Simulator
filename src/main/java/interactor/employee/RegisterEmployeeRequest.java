8a8ebf80e1062a98489863893a0b7746dc89f4b6
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-05 16:04:46 -0400

    Remove name

--- src/main/java/interactor/employee/RegisterEmployeeRequest.java
+++ src/main/java/interactor/employee/RegisterEmployeeRequest.java
@@ -6,31 +6,16 @@ package interactor.employee;
 public class RegisterEmployeeRequest {
 
     /**
-     * The name of the employee
-     */
-    public final String name;
-
-    /**
      * The type of the employee
      */
     public final EmployeeType type;
 
     /**
      * Create a new RegisterEmployeeRequest object
-     * @param name The name of the employee
      * @param type The type of the employee
      */
-    public RegisterEmployeeRequest(String name, EmployeeType type) {
-        this.name = name;
+    public RegisterEmployeeRequest(EmployeeType type) {
         this.type = type;
-    }
-
-    /**
-     * Get the name of the employee
-     * @return The name of the employee
-     */
-    public String getName() {
-        return name;
     }
 
     /**
