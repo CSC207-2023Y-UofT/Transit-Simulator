03b07ceff763e309099fe59f065f61566433191e
Charles Cheung <charles.cheung@mail.utoronto.ca>
2023-08-05 14:57:57 -0400

    Changed price-changing functionality, modified javadocs

--- src/main/java/ticket/AdultTicket.java
+++ src/main/java/ticket/AdultTicket.java
@@ -13,16 +13,16 @@ public class AdultTicket extends AbstractNormalTicket {
      * This is a constant value representing 1 day (24 hours).
      */
     public static final long TICKET_LIFETIME = 1000 * 60 * 60 * 24L;
-    public static double PRICE = 3.35;
+
+    /**
+     * The price of the AdultTicket.
+     */
+    public static final double PRICE = 3.35;
 
     /**
      * Constructs a new AdultTicket object with the predetermined type, price and ticket lifetime.
      */
     public AdultTicket() {
         super("Adult", PRICE, TICKET_LIFETIME);
-    } // TODO code smell: price modification
-
-    public void updatePrice(double price){
-        AdultTicket.PRICE = price;
     }
 }
