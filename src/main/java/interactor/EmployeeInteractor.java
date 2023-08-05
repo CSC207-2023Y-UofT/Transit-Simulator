b69ab4d94a401c143383f8a06af6816e0ccc849d
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-05 14:21:58 -0400

    save work

---
+++ src/main/java/interactor/EmployeeInteractor.java
@@ -0,0 +1,13 @@
+package interactor;
+
+import employee.EmployeeTracker;
+
+public class EmployeeInteractor {
+    private final EmployeeTracker tracker;
+
+    public EmployeeInteractor(EmployeeTracker tracker) {
+        this.tracker = tracker;
+    }
+
+
+}
