67ba94ab45447539aa264f65d84bd1271bc20ff8
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-06 12:49:02 -0400

    Fix some problems

--- src/main/java/ui/staff/admin/Management.java
+++ src/main/java/ui/staff/admin/Management.java
@@ -20,6 +20,9 @@ import java.awt.event.ActionListener;
  */
 public class Management extends JPanel {
 
+    private JPanel middlePanel;
+    private JPanel statsPanel;
+
     /**
      * Constructs a new Management object.
      *
