b27a94053af63c2a0ffc707c606e87e798d07f1a
gracelliu <gracel.liu@mail.utoronto.ca>
2023-08-06 15:54:20 -0400

    made activate button function independently for each ticket

--- src/main/java/ui/passenger/TicketPanel.java
+++ src/main/java/ui/passenger/TicketPanel.java
@@ -1,5 +1,7 @@
 package ui.passenger;
 
+import ui.round.RoundedButton;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,6 +11,7 @@ import java.util.Random;
 public class TicketPanel extends JPanel {
 
     public TicketPanel() {
+
 
         // Title
         JLabel title = new JLabel("Train Ticket", SwingConstants.CENTER);
@@ -29,13 +32,13 @@ public class TicketPanel extends JPanel {
         validTime.setFont(new Font("Serif", Font.PLAIN, 25));
 
         // Ticket Type
-        String ticketType = "Single";   // temporary
+        String ticketType = "Single";   // TODO: get ticket type from database
         JLabel typeLabel = new JLabel("Ticket Type: " + ticketType, SwingConstants.CENTER);
         typeLabel.setFont(new Font("Serif", Font.PLAIN, 25));
 
         // Timer code: DO NOT MODIFY!!
         Timer timer = new Timer(1000, new ActionListener() {
-            int remaining = 2 * 60 * 60; // 2 hours in seconds
+            int remaining = 2; // * 60 * 60; // 2 hours in seconds
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,14 +54,31 @@ public class TicketPanel extends JPanel {
                 }
             }
         });
-        timer.start();
+        
+        
+        // Activate button
+        JButton activateButton = new RoundedButton("Activate");
+        activateButton.setBackground(new Color(215, 215, 215));
+        activateButton.setFont(new Font("Serif", Font.PLAIN, 25));
+        activateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                timer.start();
+                activateButton.setEnabled(false);
+                activateButton.setBackground(new Color(255, 255, 255));
+                activateButton.setText("Active");
+            }
+        });
 
+        this.setLayout(new GridLayout(6, 1));
         this.setBackground(new Color(185, 151, 144));
+
         this.add(title);
         this.add(message);
         this.add(ticketId);
         this.add(typeLabel);
         this.add(validTime);
+        this.add(activateButton);
     }
 
     public static void main(String[] args) {
