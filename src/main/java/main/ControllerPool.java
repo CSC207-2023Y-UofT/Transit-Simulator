592c90252c710a0c6334f20ef922fe1507feef42
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-06 23:04:04 -0400

    documentation for ControllerPool and InteractorPool

--- src/main/java/main/ControllerPool.java
+++ src/main/java/main/ControllerPool.java
@@ -3,19 +3,43 @@ package main;
 import controller.stats.StatsController;
 import controller.ticket.TicketController;
 
+/**
+ * This class acts as a centralized repository for all controllers in the system.
+ * By using the InteractorPool, the ControllerPool initializes all the controllers and provides access methods to them.
+ */
 public class ControllerPool {
+
+    /** The ticket-related controller. */
     private final TicketController ticketController;
+
+    /** The stats-related controller. */
     private final StatsController statController;
 
+    /**
+     * Constructs a new ControllerPool using the provided InteractorPool.
+     * Initializes the ticket and stats controllers.
+     *
+     * @param pool The InteractorPool used for creating the controllers.
+     */
     public ControllerPool(InteractorPool pool) {
         this.ticketController = new TicketController(pool.getTicketInteractor());
         statController = new StatsController(pool.getStatInteractor());
     }
 
+    /**
+     * Provides access to the TicketController.
+     *
+     * @return The TicketController instance.
+     */
     public TicketController getTicketController() {
         return ticketController;
     }
 
+    /**
+     * Provides access to the StatsController.
+     *
+     * @return The StatsController instance.
+     */
     public StatsController getStatController() {
         return statController;
     }
--- src/main/java/main/InteractorPool.java
+++ src/main/java/main/InteractorPool.java
@@ -7,32 +7,38 @@ import interactor.ticket.ITicketInteractor;
 import interactor.train.ITrainInteractor;
 
 /**
- * The InteractorPool dataclass represents a collection of a stationInteractor and a trainInteractor.
+ * The InteractorPool class acts as a centralized repository for all interactors in the system.
+ * It provides methods to access different interactors that control distinct functionalities within the system.
  */
 public class InteractorPool {
-    /**
-     * The station interactor.
-     */
+
+    /** The station-related interactor. */
     private final IStationInteractor stationInteractor;
-    /**
-     * The train interactor.
-     */
+
+    /** The train-related interactor. */
     private final ITrainInteractor trainInteractor;
 
+    /** The ticket-related interactor. */
     private final ITicketInteractor ticketInteractor;
+
+    /** The employee-related interactor. */
     private final IEmployeeInteractor employeeInteractor;
+
+    /** The statistics-related interactor. */
     private final IStatInteractor statInteractor;
 
     /**
-     * Constructs a new InteractorPool with the given station and train interactors.
+     * Constructs a new InteractorPool with the given interactors.
      *
-     * @param stationInteractor  The StationInteractor.
-     * @param trainInteractor    The TrainInteractor.
-     * @param ticketInteractor   The TicketInteractor.
-     * @param employeeInteractor The EmployeeInteractor.
-     * @param statInteractor
+     * @param stationInteractor  The object controlling station-related operations.
+     * @param trainInteractor    The object controlling train-related operations.
+     * @param ticketInteractor   The object controlling ticket-related operations.
+     * @param employeeInteractor The object controlling employee-related operations.
+     * @param statInteractor     The object controlling statistical operations.
      */
-    public InteractorPool(IStationInteractor stationInteractor, ITrainInteractor trainInteractor, ITicketInteractor ticketInteractor, IEmployeeInteractor employeeInteractor, IStatInteractor statInteractor) {
+    public InteractorPool(IStationInteractor stationInteractor, ITrainInteractor trainInteractor,
+                          ITicketInteractor ticketInteractor, IEmployeeInteractor employeeInteractor,
+                          IStatInteractor statInteractor) {
         this.stationInteractor = stationInteractor;
         this.trainInteractor = trainInteractor;
         this.ticketInteractor = ticketInteractor;
@@ -41,31 +47,46 @@ public class InteractorPool {
     }
 
     /**
-     * Gets the station interactor.
+     * Provides access to the StationInteractor.
      *
-     * @return The station interactor.
+     * @return The StationInteractor instance.
      */
     public IStationInteractor getStationInteractor() {
         return stationInteractor;
     }
 
     /**
-     * Gets the train interactor.
+     * Provides access to the TrainInteractor.
      *
-     * @return The train interactor.
+     * @return The TrainInteractor instance.
      */
     public ITrainInteractor getTrainInteractor() {
         return trainInteractor;
     }
 
+    /**
+     * Provides access to the TicketInteractor.
+     *
+     * @return The TicketInteractor instance.
+     */
     public ITicketInteractor getTicketInteractor() {
         return ticketInteractor;
     }
 
+    /**
+     * Provides access to the EmployeeInteractor.
+     *
+     * @return The EmployeeInteractor instance.
+     */
     public IEmployeeInteractor getEmployeeInteractor() {
         return employeeInteractor;
     }
 
+    /**
+     * Provides access to the StatInteractor.
+     *
+     * @return The StatInteractor instance.
+     */
     public IStatInteractor getStatInteractor() {
         return statInteractor;
     }
