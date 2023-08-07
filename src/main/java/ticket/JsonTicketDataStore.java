b833e5629c4518abf701e53bf050b7cf09679cca
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-07 00:12:51 -0400

    Fix more tests along with a bug in JsonTicketDataStore.java

--- src/main/java/ticket/JsonTicketDataStore.java
+++ src/main/java/ticket/JsonTicketDataStore.java
@@ -31,6 +31,7 @@ public class JsonTicketDataStore implements TicketDataStore {
             Ticket ticket = new Ticket(id, type);
             ticket.setActivated(json.getBoolean("activated"));
             ticket.setExpiry(json.getLong("expiry"));
+            ticket.setCreatedAt(json.optLong("createdAt", System.currentTimeMillis()));
             return Optional.of(ticket);
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,6 +46,7 @@ public class JsonTicketDataStore implements TicketDataStore {
             json.put("id", ticket.getId());
             json.put("activated", ticket.isActivated());
             json.put("expiry", ticket.getExpiry());
+            json.put("createdAt", ticket.getCreatedAt());
             Files.writeString(file.toPath(), json.toString());
         } catch (Exception e) {
             e.printStackTrace();
