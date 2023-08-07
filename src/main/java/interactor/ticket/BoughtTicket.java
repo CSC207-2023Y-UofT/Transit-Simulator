c0dce8d4fdf14a199b41cdb6cd7ad50b08a3bbe6
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-06 22:56:53 -0400

    documentation for interactor/ticket

--- src/main/java/interactor/ticket/BoughtTicket.java
+++ src/main/java/interactor/ticket/BoughtTicket.java
@@ -2,13 +2,36 @@ package interactor.ticket;
 
 import ticket.TicketType;
 
+/**
+ * Represents a purchased ticket in the system.
+ * This class provides details related to a bought ticket including its cost, type, activation status, and expiry time.
+ */
 public class BoughtTicket {
+
+    /** The cost of the bought ticket. */
     private final double cost;
+
+    /** The type of the ticket. */
     private final TicketType type;
+
+    /** The unique identifier for the ticket. */
     private final int ticketId;
+
+    /** Indicates if the ticket has been activated. */
     private boolean activated;
+
+    /** Timestamp indicating when the ticket will expire. */
     private long expiry;
 
+    /**
+     * Constructs a new BoughtTicket with the specified attributes.
+     *
+     * @param cost      The cost of the ticket.
+     * @param type      The type of the ticket.
+     * @param ticketId  The unique identifier for the ticket.
+     * @param activated Indicates if the ticket has been activated.
+     * @param expiry    Timestamp indicating when the ticket will expire.
+     */
     public BoughtTicket(double cost, TicketType type, int ticketId,
                         boolean activated, long expiry) {
         this.cost = cost;
@@ -18,22 +41,47 @@ public class BoughtTicket {
         this.expiry = expiry;
     }
 
+    /**
+     * Checks if the ticket has been activated.
+     *
+     * @return {@code true} if the ticket is activated, {@code false} otherwise.
+     */
     public boolean isActivated() {
         return activated;
     }
 
+    /**
+     * Retrieves the expiry timestamp of the ticket.
+     *
+     * @return The expiry timestamp.
+     */
     public long getExpiry() {
         return expiry;
     }
 
+    /**
+     * Retrieves the cost of the ticket.
+     *
+     * @return The cost of the ticket.
+     */
     public double getCost() {
         return cost;
     }
 
+    /**
+     * Retrieves the type of the ticket.
+     *
+     * @return The type of the ticket.
+     */
     public TicketType getType() {
         return type;
     }
 
+    /**
+     * Retrieves the unique identifier of the ticket.
+     *
+     * @return The ticket ID.
+     */
     public int getTicketId() {
         return ticketId;
     }
--- src/main/java/interactor/ticket/TicketInteractor.java
+++ src/main/java/interactor/ticket/TicketInteractor.java
@@ -8,15 +8,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * This class serves as an intermediary between the system and ticket-related operations,
+ * facilitating actions such as buying and activating tickets.
+ */
 public class TicketInteractor implements ITicketInteractor {
+
+    /** Controller for recording statistics about ticket sales. */
     private final StatDataController stats;
+
+    /** Data store to manage and store ticket information. */
     private final TicketDataStore dataStore;
 
+    /**
+     * Constructs a new TicketInteractor with a specified ticket data store and stats controller.
+     *
+     * @param dataStore The data store for ticket-related data.
+     * @param stats     The stats controller to record ticket statistics.
+     */
     public TicketInteractor(TicketDataStore dataStore, StatDataController stats) {
         this.dataStore = dataStore;
         this.stats = stats;
     }
 
+    /**
+     * Buys tickets for the given ticket types.
+     *
+     * @param ticketTypes List of ticket types to be bought.
+     * @return List of bought tickets.
+     */
     public List<BoughtTicket> buyTickets(List<TicketType> ticketTypes) {
 
         List<Ticket> tickets = new ArrayList<>();
@@ -43,6 +63,12 @@ public class TicketInteractor implements ITicketInteractor {
         return response;
     }
 
+    /**
+     * Retrieves the information of a bought ticket by its ID.
+     *
+     * @param ticketId ID of the ticket to be retrieved.
+     * @return An optional containing the bought ticket information if found, empty otherwise.
+     */
     @Override
     public Optional<BoughtTicket> getTicket(int ticketId) {
         Ticket ticket = dataStore.getTicket(ticketId).orElse(null);
@@ -50,6 +76,12 @@ public class TicketInteractor implements ITicketInteractor {
         return Optional.of(convertTicket(ticket));
     }
 
+    /**
+     * Activates a ticket based on its ID and returns its information.
+     *
+     * @param ticketId ID of the ticket to be activated.
+     * @return An optional containing the activated ticket information if found and activated, empty otherwise.
+     */
     @Override
     public Optional<BoughtTicket> activateTicket(int ticketId) {
         Ticket ticket = dataStore.getTicket(ticketId).orElse(null);
@@ -64,6 +96,12 @@ public class TicketInteractor implements ITicketInteractor {
                 ticket.getExpiry()));
     }
 
+    /**
+     * Converts a Ticket object to a BoughtTicket object.
+     *
+     * @param ticket The Ticket object to be converted.
+     * @return The converted BoughtTicket object.
+     */
     private BoughtTicket convertTicket(Ticket ticket) {
         return new BoughtTicket(ticket.getPrice(),
                 ticket.getType(),
