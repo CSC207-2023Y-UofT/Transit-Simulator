4aab344e3d43e49f09330557f08ace2a2cd44f0c
Matthew Lack <matt.lack@mail.utoronto.ca>
2023-08-07 22:37:19 -0400

    Add javadocs to TicketController.java

--- src/main/java/controller/ticket/TicketController.java
+++ src/main/java/controller/ticket/TicketController.java
@@ -7,22 +7,46 @@ import ticket.TicketType;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Controller, basically just a delegator to an ITicketInteractor instance
+ */
 public class TicketController {
 
+    /**
+     * The ticket interactor
+     */
     private final ITicketInteractor interactor;
 
+    /**
+     * The constructor
+     */
     public TicketController(ITicketInteractor interactor) {
         this.interactor = interactor;
     }
 
+    /**
+     * Buys tickets
+     * @param ticketTypes The types of tickets to buy
+     * @return The list of bought tickets
+     */
     public List<BoughtTicket> buyTickets(List<TicketType> ticketTypes) {
         return interactor.buyTickets(ticketTypes);
     }
 
+    /**
+     * Activates a ticket
+     * @param ticketId The id of the ticket to activate
+     * @return The activated ticket, or empty if the ticket was not found
+     */
     public Optional<BoughtTicket> activateTicket(int ticketId) {
         return interactor.activateTicket(ticketId);
     }
 
+    /**
+     * Gets a ticket
+     * @param ticketId The id of the ticket to get
+     * @return The ticket, or empty if the ticket was not found
+     */
     public Optional<BoughtTicket> getTicket(int ticketId) {
         return interactor.getTicket(ticketId);
     }
