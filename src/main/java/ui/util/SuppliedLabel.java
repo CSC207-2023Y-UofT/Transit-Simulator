2ce6e8db1ff5b12f1144f4b51b48699e118219ba
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-09 17:48:49 -0400

    documentation for SuppliedLabel and SuppliedRoundLabel

--- src/main/java/ui/util/SuppliedLabel.java
+++ src/main/java/ui/util/SuppliedLabel.java
@@ -4,10 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.function.Supplier;
 
+/**
+ * SuppliedLabel is a JLabel that gets its text from a Supplier.
+ */
 public class SuppliedLabel extends JLabel {
 
+    /**
+     * The Supplier that is used to get the text.
+     */
     private final Supplier<String> textSupplier;
 
+    /**
+     * Constructs a new SuppliedLabel object with the given Supplier.
+     *
+     * @param textSupplier the Supplier that is used to get the text
+     */
     public SuppliedLabel(Supplier<String> textSupplier) {
         super("");
         this.textSupplier = textSupplier;
@@ -18,4 +29,5 @@ public class SuppliedLabel extends JLabel {
         setText(textSupplier.get());
         super.paintComponent(g);
     }
+
 }
