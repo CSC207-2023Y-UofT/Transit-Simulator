3a4c7ca8f29d43366b8acfc233c4b06cf9d9d53b
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-09 17:48:31 -0400

    documentation for ShadowPanel

--- src/main/java/ui/util/ShadowPanel.java
+++ src/main/java/ui/util/ShadowPanel.java
@@ -3,10 +3,21 @@ package ui.util;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * ShadowPanel is a JPanel that has a shadow.
+ */
 public class ShadowPanel extends JPanel {
 
+    /**
+     * The ShadowBorder that is used to draw the shadow.
+     */
     private final ShadowBorder shadowBorder;
 
+    /**
+     * Constructs a new ShadowPanel object with the given LayoutManager.
+     *
+     * @param layoutManager the LayoutManager that is used to layout the components
+     */
     public ShadowPanel(LayoutManager layoutManager) {
         super(layoutManager);
         setBackground(new Color(0, 0, 0, 0));
@@ -14,6 +25,9 @@ public class ShadowPanel extends JPanel {
         setBorder(shadowBorder);
     }
 
+    /**
+     * Constructs a new ShadowPanel object with the default FlowLayout.
+     */
     public ShadowPanel() {
         super();
         setBackground(new Color(0, 0, 0, 0));
@@ -21,10 +35,20 @@ public class ShadowPanel extends JPanel {
         setBorder(shadowBorder);
     }
 
+    /**
+     * Sets the extra inset of the shadow.
+     *
+     * @param extraInset the extra inset of the shadow
+     */
     public void setExtraInset(int extraInset) {
         shadowBorder.setExtraInsets(extraInset, extraInset, extraInset, extraInset);
     }
 
+    /**
+     * Sets the thickness of the shadow border.
+     *
+     * @param thickness the thickeness of the shadow border.
+     */
     public void setThickness(int thickness) {
         shadowBorder.setThickness(thickness);
     }
