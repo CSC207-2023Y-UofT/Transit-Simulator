96680762750625b3e6a4c1436d23b50b3f89c89a
Grace Liu <gracel.liu@mail.utoronto.ca>
2023-08-09 20:10:58 -0400

    delete unused imports

--- src/main/java/ui/map/StationPanel.java
+++ src/main/java/ui/map/StationPanel.java
@@ -5,8 +5,6 @@ import ui.util.SuppliedLabel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
