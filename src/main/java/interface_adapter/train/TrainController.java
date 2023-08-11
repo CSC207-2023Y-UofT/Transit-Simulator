cb8ce5761fb953f25bc97318c7ff252886b57336
Charles Cheung <charlescheung22@gmail.com>
2023-08-10 23:46:53 -0400

    Added javadocs

--- src/main/java/interface_adapter/train/TrainController.java
+++ src/main/java/interface_adapter/train/TrainController.java
@@ -5,13 +5,30 @@ import app_business.dto.TrainDTO;
 
 import java.util.Optional;
 
+/**
+ * The TrainController class represents the controller for the train entity.
+ */
 public class TrainController {
+
+    /**
+     * The interactor for the train entity.
+     */
     private final ITrainInteractor interactor;
 
+    /**
+     * Constructs a new TrainController with the given interactor.
+     *
+     * @param interactor The interactor for the train entity.
+     */
     public TrainController(ITrainInteractor interactor) {
         this.interactor = interactor;
     }
 
+    /**
+     * Finds a train by its name.
+     * @param name The name of the train.
+     * @return An optional containing the train if it exists, an empty optional otherwise.
+     */
     public Optional<TrainDTO> find(String name) {
         return interactor.find(name);
     }
