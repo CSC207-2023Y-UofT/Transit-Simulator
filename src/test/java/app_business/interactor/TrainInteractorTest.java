595f1166ebcc9155d89f56447fc9a60e287dbf7a
Charles Cheung <charlescheung22@gmail.com>
2023-08-12 09:01:06 -0400

    finished last test

--- src/test/java/app_business/interactor/TrainInteractorTest.java
+++ src/test/java/app_business/interactor/TrainInteractorTest.java
@@ -1,7 +1,12 @@
 package app_business.interactor;
 
 import app_business.dto.TrainDTO;
+import entity.model.Direction;
 import entity.model.control.TransitModel;
+import entity.model.node.Node;
+import entity.model.node.line.NodeLineProfile;
+import entity.model.node.station.StationFactory;
+import entity.model.train.Train;
 import entity.model.train.TrainStatus;
 import entity.model.train.track.TrackSegment;
 
@@ -58,10 +63,62 @@ public class TrainInteractorTest {
         Assertions.assertEquals(TrainStatus.IN_SERVICE, model.getTrain("train1").getStatus());
     }
 
-    @Disabled
     @Test
     public void testToDTO() {
-        // TODO: Implement this test.
+        // Scenario setup (other tests don't need this)
+        StationFactory factory = new StationFactory();
+        Node station1 = model.createNode(factory, "station1");  // Three stations linked in a line.
+        Node station2 = model.createNode(factory, "station2");
+        Node station3 = model.createNode(factory, "station3");
+        station1.setX(100);
+        station1.setY(150);
+        station2.setX(200);
+        station2.setY(250);
+        station3.setX(300);
+        station3.setY(350);
+        NodeLineProfile l1s1 = station1.createLineProfile(1);
+        NodeLineProfile l1s2 = station2.createLineProfile(1);
+        NodeLineProfile l1s3 = station3.createLineProfile(1);
+        TrackSegment s1f = l1s1.getTrack(Direction.FORWARD);
+        TrackSegment s2f = l1s2.getTrack(Direction.FORWARD);
+        TrackSegment s3f = l1s3.getTrack(Direction.FORWARD);
+        TrackSegment s1b = l1s1.getTrack(Direction.BACKWARD);
+        TrackSegment s2b = l1s2.getTrack(Direction.BACKWARD);
+        TrackSegment s3b = l1s3.getTrack(Direction.BACKWARD);
+        TrackSegment t1f = new TrackSegment(model.getTrackRepo(), "t1f", 100);
+        TrackSegment t2f = new TrackSegment(model.getTrackRepo(), "t2f", 100);
+        TrackSegment t1b = new TrackSegment(model.getTrackRepo(), "t1b", 100);
+        TrackSegment t2b = new TrackSegment(model.getTrackRepo(), "t2b", 100);
+        model.getTrackRepo().addTrack(t1f);
+        model.getTrackRepo().addTrack(t2f);
+        model.getTrackRepo().addTrack(t1b);
+        model.getTrackRepo().addTrack(t2b);
+        s1f.linkForward(t1f);
+        t1f.linkForward(s2f);
+        s2f.linkForward(t2f);
+        t2f.linkForward(s3f);
+        s3b.linkForward(t2b);
+        t2b.linkForward(s2b);
+        s2b.linkForward(t1b);
+        t1b.linkForward(s1b);
+        Train trainForwards = model.createTrain(s2f, "trainForwards", 120);
+        trainForwards.setStatus(TrainStatus.IN_SERVICE);
+
+        // Test
+        TrainDTO dto = TrainInteractor.toDTO(trainForwards);
+        Assertions.assertEquals("trainForwards", dto.getName());
+        Assertions.assertEquals(120, dto.getCapacity());
+        Assertions.assertEquals(0, dto.getOccupation());
+        Assertions.assertTrue(dto.getCurrentStation().isPresent());
+        Assertions.assertEquals("station2", dto.getCurrentStation().get().getName());
+        Assertions.assertTrue(dto.getNextNodeDistance().isPresent());
+        Assertions.assertEquals("station3", dto.getNextNodeDistance().get().getStation().getName());
+        Assertions.assertEquals(200, dto.getNextNodeDistance().get().getDistance());  // 200 = 100 + 100
+        Assertions.assertTrue(dto.getPreviousNodeDistance().isPresent());
+        Assertions.assertEquals("station1", dto.getPreviousNodeDistance().get().getStation().getName());
+        Assertions.assertEquals(100, dto.getPreviousNodeDistance().get().getDistance());
+        Assertions.assertEquals(TrainStatus.IN_SERVICE, dto.getStatus());
+
     }
 }
 
